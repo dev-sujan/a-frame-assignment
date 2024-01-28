dc486e2d843d8e32daeee236395f420a89918335
Sujan Maji <dev.sujanmaji@gmail.com>
2024-01-28 15:12:46 +0530

    remove on-hover-rotate

--- scroll-hold-rotation.js
+++ scroll-hold-rotation.js
@@ -10,7 +10,6 @@ AFRAME.registerComponent("scroll-hold-rotation", {
     document.addEventListener("mousedown", this.onMouseDown.bind(this));
     document.addEventListener("mousemove", this.onMouseMove.bind(this));
     document.addEventListener("mouseup", this.onMouseUp.bind(this));
-
   },
 
   onMouseDown: function (event) {
@@ -50,52 +49,6 @@ AFRAME.registerComponent("scroll-hold-rotation", {
     if (event.button === 1) {
       this.isRotating = false;
       console.log("Click up");
-    }
-  },
-});
-
-AFRAME.registerComponent("rotate-on-hover", {
-  init: function () {
-    // Flag to track if the component is being hovered
-    this.isHovered = false;
-
-    // Store the current rotation state
-    this.currentRotation = { x: 0, y: 0, z: 0 };
-
-    // Add event listeners for mouseenter and mouseleave events
-    this.el.addEventListener("mouseenter", this.onMouseEnter.bind(this));
-    this.el.addEventListener("mouseleave", this.onMouseLeave.bind(this));
-
-    // Add an event listener to the document for mousemove events
-    document.addEventListener("mousemove", this.onMouseMove.bind(this));
-  },
-
-  onMouseEnter: function () {
-    // Set the hover flag to true when the mouse enters the component
-    this.isHovered = true;
-  },
-
-  onMouseLeave: function () {
-    // Set the hover flag to false when the mouse leaves the component
-    this.isHovered = false;
-  },
-
-  onMouseMove: function (event) {
-    // Check if the component is being hovered
-    if (this.isHovered) {
-      // Calculate the rotation angles based on the mouse position
-      const rotationX = (event.clientY / window.innerHeight) * 360;
-      const rotationY = (event.clientX / window.innerWidth) * 360;
-
-      // Update the current rotation state
-      this.currentRotation = {
-        x: rotationX,
-        y: rotationY,
-        z: 0,
-      };
-
-      // Set the rotation of the object
-      this.el.setAttribute("rotation", this.currentRotation);
     }
   },
 });
